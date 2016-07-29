b8776b9a5ad60c58e02822c2eb92dc31a5de4c76
Jack Zhu <jackzhu@stanford.edu>
2016-07-28 17:50:56 -0700

    hmmmmmm idk man

--- lookout-targetland/lookout-targetland.py
+++ lookout-targetland/lookout-targetland.py
@@ -2,59 +2,72 @@
 # -*- coding: utf-8 -*-
 import code
 
-from pymavlink import mavutil # Needed for command message definitions
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 import time, sys
 
-try:
-    target = sys.argv[1] if len(sys.argv) >= 2 else 'tcp:127.0.0.1:5762'  #'udpin:0.0.0.0:14550'
-    print 'Connecting to ' + target + '...'
-    vehicle = connect(target, wait_ready=True)
-except Exception:
-    target = sys.argv[1] if len(sys.argv) >= 2 else 'udpin:0.0.0.0:14550'  #'udpin:0.0.0.0:14550'
-    print 'Connecting to ' + target + '...'
-    vehicle = connect(target, wait_ready=True)
+# Global vehicle object represents drone or simulator
+vehicle = None
+homelocation_hacked = None
+homelocation_local_hacked = None
 
-# Reference for all
-print "Autopilot Firmware version: %s" % vehicle.version
-# print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
-print "Global Location: %s" % vehicle.location.global_frame
-print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
-print "Local Location: %s" % vehicle.location.local_frame    #NED
-print "Attitude: %s" % vehicle.attitude
-print "Velocity: %s" % vehicle.velocity
-print "GPS: %s" % vehicle.gps_0
-print "Groundspeed: %s" % vehicle.groundspeed
-print "Airspeed: %s" % vehicle.airspeed
-print "Gimbal status: %s" % vehicle.gimbal
-print "Battery: %s" % vehicle.battery
-print "EKF OK?: %s" % vehicle.ekf_ok
-print "Last Heartbeat: %s" % vehicle.last_heartbeat
-print "Rangefinder: %s" % vehicle.rangefinder
-print "Rangefinder distance: %s" % vehicle.rangefinder.distance
-print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
-print "Heading: %s" % vehicle.heading
-print "Is Armable?: %s" % vehicle.is_armable
-print "System status: %s" % vehicle.system_status.state
-print "Mode: %s" % vehicle.mode.name    # settable
-print "Armed: %s" % vehicle.armed    # settable
+def start_connect():
+    global vehicle
+    global homelocation_hacked
+    global homelocation_local_hacked
 
-# cmds = vehicle.commands
-# cmds.download()
-# cmds.wait_ready()
+    print 'Begin Connect'
+    try:
+        target = sys.argv[1] if len(sys.argv) >= 2 else 'tcp:127.0.0.1:5762'  #'udpin:0.0.0.0:14550'
+        print 'Connecting to ' + target + '...'
+        vehicle = connect(target, wait_ready=True)
+    except Exception:
+        print 'No simulator present, connecting to drone'
+        target = sys.argv[1] if len(sys.argv) >= 2 else 'udpin:0.0.0.0:14550'  #'udpin:0.0.0.0:14550'
+        print 'Connecting to ' + target + '...'
+        vehicle = connect(target, wait_ready=True)
 
-# FIX THE ACTUAL HOME LOCATION
-homelocation_hacked = vehicle.location.global_frame
-homelocation_local_hacked = vehicle.location.local_frame
-print " Home Location: %s" % homelocation_hacked
-print " Home Location Local: %s" % homelocation_local_hacked
+    # Reference for all commands/attributes
+    print "Autopilot Firmware version: %s" % vehicle.version
+    # print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
+    print "Global Location: %s" % vehicle.location.global_frame
+    print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
+    print "Local Location: %s" % vehicle.location.local_frame    #NED
+    print "Attitude: %s" % vehicle.attitude
+    print "Velocity: %s" % vehicle.velocity
+    print "GPS: %s" % vehicle.gps_0
+    print "Groundspeed: %s" % vehicle.groundspeed
+    print "Airspeed: %s" % vehicle.airspeed
+    print "Gimbal status: %s" % vehicle.gimbal
+    print "Battery: %s" % vehicle.battery
+    print "EKF OK?: %s" % vehicle.ekf_ok
+    print "Last Heartbeat: %s" % vehicle.last_heartbeat
+    print "Rangefinder: %s" % vehicle.rangefinder
+    print "Rangefinder distance: %s" % vehicle.rangefinder.distance
+    print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
+    print "Heading: %s" % vehicle.heading
+    print "Is Armable?: %s" % vehicle.is_armable
+    print "System status: %s" % vehicle.system_status.state
+    print "Mode: %s" % vehicle.mode.name    # settable
+    print "Armed: %s" % vehicle.armed    # settable
 
-# code.interact(local=locals())
+    # cmds = vehicle.commands
+    # cmds.download()
+    # cmds.wait_ready()
+
+    # TODO: FIX THE ACTUAL HOME LOCATION
+    homelocation_hacked = vehicle.location.global_frame
+    homelocation_local_hacked = vehicle.location.local_frame
+    print " Home Location: %s" % homelocation_hacked
+    print " Home Location Local: %s" % homelocation_local_hacked
+
 
 def arm_and_takeoff(aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
     """
+    global vehicle, homelocation_hacked, homelocation_local_hacked
+
+    print "Begin arm and takeoff"
 
     print "Basic pre-arm checks"
     # Don't try to arm until autopilot is ready
@@ -87,11 +100,15 @@ def arm_and_takeoff(aTargetAltitude):
 
 # Return relative North, East offset of landing zone
 def acquire_target_NED_location():
+    global vehicle, homelocation_hacked, homelocation_local_hacked
+
     dN = vehicle.location.local_frame.north - homelocation_local_hacked.north
     dE = vehicle.location.local_frame.east - homelocation_local_hacked.east
     return (dN + 1, dE + 1)
 
 def land_control_velocity(dN, dE):
+    global vehicle, homelocation_hacked, homelocation_local_hacked
+
     threshold = .1
     v = .1
     v_n = 0
@@ -111,6 +128,10 @@ def land_control_velocity(dN, dE):
     return (v_n, v_e, v_d)
 
 def precision_land():
+    global vehicle, homelocation_hacked, homelocation_local_hacked
+
+    print "Begin Precision Land"
+
     print "Going home to Home Location: %s" % homelocation_hacked
     vehicle.gimbal.rotate(-90,0,0)
     above_home_loc = LocationGlobalRelative(homelocation_hacked.lat, \
@@ -118,7 +139,7 @@ def precision_land():
                                             10)
     vehicle.simple_goto(above_home_loc)
 
-    # Put loop here
+    # TODO Put loop here
     time.sleep(10)
 
     print "Lowering to 5 m"
@@ -128,10 +149,8 @@ def precision_land():
 
     vehicle.simple_goto(above_home_loc, groundspeed=.3)
 
-    # Loop here
+    # TODO Loop here
     time.sleep(10)
-
-
     send_ned_velocity(0, 0, .1)
 
     while(True):
@@ -162,13 +181,14 @@ def precision_land():
 #     vehicle.flush()
 
 def send_ned_velocity(velocity_x, velocity_y, velocity_z):
+    global vehicle, homelocation_hacked, homelocation_local_hacked
     """
     Move vehicle in direction based on specified velocity vectors.
     """
     msg = vehicle.message_factory.set_position_target_local_ned_encode(
         0,       # time_boot_ms (not used)
         0, 0,    # target system, target component
-        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        1, # NED frame constant
         0b0000111111000111, # type_mask (only speeds enabled)
         0, 0, 0, # x, y, z positions (not used)
         velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
@@ -177,9 +197,18 @@ def send_ned_velocity(velocity_x, velocity_y, velocity_z):
 
     vehicle.send_mavlink(msg)
 
-arm_and_takeoff(5)
-time.sleep(10)
-precision_land()
+# Runtime Script:
+# code.interact(local=locals())
 
-print "Close vehicle object"
-vehicle.close()
+start_connect()
+
+if vehicle is not None:
+    if homelocation_hacked is not None and homelocation_local_hacked is not None:
+        arm_and_takeoff(5)
+        time.sleep(10)
+        precision_land()
+
+    print "Close vehicle object"
+    vehicle.close()
+else:
+    print "Vehicle was none??"
